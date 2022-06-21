bdd830eaf3e8c59319f173605149a63db1008c9e
quocphu1997 <congaquevip@gmail.com>
2022-06-21 22:09:45 +0700

    modify component file name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Components/Header";
-import Body from "./Components/Body";
-import Footer from "./Components/Footer";
+import Header from "./BaiTapThucHanhLayout/Header";
+import Body from "./BaiTapThucHanhLayout/Body";
+import Footer from "./BaiTapThucHanhLayout/Footer";
 
 function App() {
   return (
